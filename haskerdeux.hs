1844824289593f9698b56ebdde7af4a65d60d0de
atomicules <atomicules@lavabit.com>
2012-03-19 13:50:21 +0000

    More work in progress

--- haskerdeux.hs
+++ haskerdeux.hs
@@ -2,59 +2,72 @@
 
 import System.Environment
 import System.IO
+import System.IO.Error
 import Data.List
+import Data.List.Split --need to install
+import Network.Curl --need to install
+--import Network.TLS --still needed with Curl?
+import Data.Word (Word32) --for use with Curl port
+import Control.Monad
+import Data.Maybe
 
-import Network.HTTP.Conduit
-import Network.TLS
-import qualified Data.ByteString.Char8 as C
-import qualified Data.ByteString.Lazy as L
-import Data.Maybe (fromJust)
-import Network.HTTP.Proxy (parseProxy)
+
 
 dispatch :: String -> [String] -> IO ()
-dispatch "today" = today
+--dispatch "today" = today
 --dispatch "new" = new
 --dispatch "complete" = complete
 dispatch "test" = test
 --dispatch "edit" = edit
 --dispatch "delete" = delete
 
+--Define function to get Env variable.
+--from http://stackoverflow.com/a/2682887/208793
+--As don't care about error, just the actual thing.
+getEnvVar x = do {
+	var <- getEnv x;
+	return (Just var);
+} `catch` \ex -> do {
+	return Nothing
+} 
 
-main = do
+
+--setproxy :: Maybe String -> [CurlOption] --very close to working, but the [CurlOption] isn't quite right
+--setproxy :: Monad m => Maybe [Char] -> m [CurlOption]
+--apparently!
+setproxy proxy = do
+	let systemproxyparts = reverse $ splitOneOf "/;:@" $ fromJust proxy --surely fromJust is ok in this scenerio?
+	let port = read(systemproxyparts !! 0)::Word32
+	let opts = if isInfixOf "@" $ fromJust proxy
+		then [ CurlProxyPort port , CurlProxy $ systemproxyparts !! 1, CurlProxyPassword  $ systemproxyparts !! 2 , CurlProxyUser $ systemproxyparts !! 3] 
+		else [ CurlProxyPort port, CurlProxy $ systemproxyparts !! 1 ]
+	return opts
+
+main = withCurlDo $ do --http://flygdynamikern.blogspot.com/2009/03/extended-sessions-with-haskell-curl.html
+
+	systemproxy <- getEnvVar "http-proxy"
+
+	when (isJust systemproxy) (setproxy systemproxy)
+
+
+	curl <- initialize
+	--setopts curl opts
+
 	(command:argList) <- getArgs
 	dispatch command argList
 
 
---logon? Don't know how to cache credentials though, just send with each command for now
-
-
-today :: [String] -> IO ()
-today [username, password] = do
-	
-	let user = C.pack username
-	let pass = C.pack password
-
-	--Proxy Support notes
-	--===================
-	--http-conduit does it in host port fashion
-	--let proxyhost = ""
-	--let proxyport = read "80"::Int --As otherwise is Integer which is wrong type
-	--which then somehow gets added like to 
-	--let req = addProxy proxy $ applyBasicAuth user pass $ fromJust $ parseUrl  "https://teuxdeux.com/api/list.json"
-	--But don't think I can do authentication like that. Need it in the form that simpleHTTP uses:
-	--let proxy = parseProxy "http://<username>:<password>@<proxyhost>:<proxyport>"
-	--But then I don't believe simpleHTTP does https?
-	let req = applyBasicAuth user pass $ fromJust $ parseUrl  "https://teuxdeux.com/api/list.json"
-	--But next problem is http-conduit and https certs. Need to do something like:
-	--let manager = newManager manSettings
-	--where 
-	--	manSettings = managerCheckCerts = \ _ _ -> return CertificateUsageAccept
-	
-	res <- withManager $ httpLbs req
-
-	L.putStrLn $ responseBody res
+--today :: [String] -> IO ()
+--today [username, password] = do
+--	let user = C.pack username
+--	let pass = C.pack password
+--	let req = applyBasicAuth user pass $ fromJust $ parseUrl  "https://teuxdeux.com/api/list.json"
+--	res <- withManager $ httpLbs req
+--	L.putStrLn $ responseBody res
 
 	--http://stackoverflow.com/a/5614946/208793
+
+
 
 
 test :: [String] -> IO ()
