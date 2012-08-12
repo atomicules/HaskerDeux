97aae57d82fd36ba129f172d8703ea2c8e065122
atomicules <atomicules@lavabit.com>
2012-08-12 23:59:53 +0100

    Work in progress...
    Not working, but just trying to flesh things out a bit more. I am
    surprised I'm even allowed the time to breathe, so trying to find
    time to tinker with this is hard.

--- haskerdeux-api.hs
+++ haskerdeux-api.hs
@@ -19,6 +19,7 @@ import System.Locale (defaultTimeLocale)
 
 api_user = "https://teuxdeux.com/api/user.json"
 api_list = "https://teuxdeux.com/api/list.json"
+api_someday = "https://teuxdeux.com/api/someday.json"
 api_update = "https://teuxdeux.com/api/update.json"
 api_todo = "https://teuxdeux.com/api/todo.json"
     
@@ -26,63 +27,62 @@ api_todo = "https://teuxdeux.com/api/todo.json"
 --to fake the approach of badboy's teuxdeux can just do:
 --let client = (username, password)
 --
-
-resp client api = withCurlDo $ do
+--curlGetString base function
+respCGS [client, api, opts] = withCurlDo $ do
 	let username = fst client
 	    password = snd client
-	    opts = [CurlUserPwd $ username++":"++password]  --http://stackoverflow.com/a/2140445/208793
-	curlGetString api opts
+	    opts' = opts++[CurlUserPwd $ username++":"++password]
+	curlGetString api opts'
 
-user client = withCurlDo $ do
-	resp client user
+user [client] = do
+	body <- respCGS client api_user []
+	decodeJSON $ snd body :: [User]
 
-
-todos client = withCurlDo $ do
-	body client list
-
-	let tds = decodeJSON $ snd body :: [Teuxdeux]
+--Need to include someday param.
+todos [client] = do
+	body <- respCGS client api_list []
+	decodeJSON $ snd body :: [Teuxdeux]
 	
 
-someday client = withCurl $ do
-	body client list
+someday [client] = do
+	body <- respCGS client api_someday []
+	decodeJSON $ snd body :: [Teuxdeux]
 
-
-
-
-create_todo client todo = withCurlDo $ do
-	let opts = method_POST ++ [CurlUserPwd $ username++":"++password, CurlPostFields ["todo_item[todo]="++todo, "todo_item[do_on]="++todays_date] ]
+--do_curl_ base function
+respDC [client, api, opts] = withCurlDo $ do
+	let username = fst client
+	    password = snd client
+	    opts' = opts++method_POST++[CurlUserPwd $ username++":"++password]
 	curl <- initialize
-	resp <- do_curl_ curl "https://teuxdeux.com/api/todo.json" opts :: IO CurlResponse
+	do_curl_ curl api :: IO CurlResponse
 
 
-
-update_todo [todays_date, username, password, number] = withCurlDo $ do
-	
-	let opts = method_POST ++ [CurlUserPwd $ username++":"++password, CurlPostFields ["todo_item["++(show itemid)++"?][done]=1"] ]
-	curl <- initialize
-	resp <- do_curl_ curl "https://teuxdeux.com/api/update.json" opts :: IO CurlResponse
+create_todo [client, todo, todo_date] = do
+	let opts = [CurlPostFields ["todo_item[todo]="++todo, "todo_item[do_on]="++todo_date] ]
+	respDC client api_todo opts 
 
 
-delete_todo [todays_date, username, password, number] = withCurlDo $ do
+update_todo [client, todo, todo_date] = do
+	let opts = [CurlPostFields ["todo_item["++todo++"?][done]=1"] ]
+	respDC client api_update opts 
 
-	let opts = method_POST ++ [CurlUserPwd $ username++":"++password, CurlPostFields ["todo_item["++(show itemid)++"?][do_on]="++tomorrows_date] ]
-	curl <- initialize
-	resp <- do_curl_ curl "https://teuxdeux.com/api/update.json" opts :: IO CurlResponse
+--Need to handle slightly differently and use HTTP DELETE
+--delete_todo client todo todo_date = do
 
 
 
 --Thanks to http://www.amateurtopologist.com/blog/2010/11/05/a-haskell-newbies-guide-to-text-json/ and http://hpaste.org/41263/parsing_json_with_textjson
 data Teuxdeux = Teuxdeux {
-    id :: Integer,
+    tdid :: Integer,
 	do_on :: String, 
 	todo :: String,
 	done :: Bool
 } deriving (Eq, Show, Data, Typeable) 
 
 data User = User {
-  id: Integer,
-  login: String,
-  email: String,
-  time_zone: String,
-  utc_offset: <timezone offset in minutes>
-}
+  uid :: Integer,
+  login :: String,
+  email :: String,
+  time_zone :: String,
+  utc_offset :: Integer
+} deriving (Eq, Show, Data, Typeable) 
