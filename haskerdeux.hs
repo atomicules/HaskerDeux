d50a404cc7c8aee9986f15d2708f359d4a39a04b
atomicules <atomicules@lavabit.com>
2012-09-05 15:16:58 +0100

    hlint changes

--- haskerdeux.hs
+++ haskerdeux.hs
@@ -30,7 +30,7 @@ main = do
 	todays_date <- fmap (formatTime defaultTimeLocale "%Y-%m-%d") getCurrentTime
 	(command:argList) <- getArgs
 	--If username and password not supplied read from netrc
-	if (command == "today" && length argList == 0) || ((command == "new" || command =="crossoff" || command == "putoff") && length argList == 1) || (command == "moveto" && length argList == 2)  
+	if (command == "today" && null argList) || (command `elem` ["new", "crossoff", "putoff"] && length argList == 1) || (command == "moveto" && length argList == 2)  
 		then do 
 			username <- fmap fst readnetrc
 			password <- fmap snd readnetrc
@@ -42,12 +42,12 @@ readnetrc = do
 	home <- getHomeDirectory
 	netrc <- fmap lines $ readFile (home ++ "/.netrc")
 	let netrc' = dropWhile (not . isInfixOf "teuxdeux") netrc
-	let (username, password) = if "login" `isInfixOf` (head netrc')
+	let (username, password) = if "login" `isInfixOf` head netrc'
 		-- if entry is on one line	
 		then (getcred "login", getcred "password") 
 		-- if entry is on multiple lines
 		else (last $ words $ netrc' !! 1, last $ words $ netrc' !! 2)
-		where getcred = \c -> (dropWhile (not . isInfixOf c) $ words $ head netrc') !! 1
+		where getcred c = dropWhile (not . isInfixOf c) (words $ head netrc') !! 1
 	return (username, password)
 
 
@@ -55,7 +55,7 @@ curlget [todays_date, username, password] = withCurlDo $ do
 	let opts1 = [CurlUserPwd $ username++":"++password] 
 	body <- curlGetString "https://teuxdeux.com/api/list.json" opts1
 	let tds = decodeJSON $ snd body :: [Teuxdeux]
-	let tdsf = filter (\td -> do_on td ==todays_date && done td == False) tds
+	let tdsf = filter (\td -> do_on td ==todays_date && not (done td)) tds
 	return tdsf
 	
 
@@ -64,7 +64,7 @@ curlpost [todays_date, curlpostdata, apiurl, okresponse, username, password] num
 		then do
 			tdsf <- curlget [todays_date, username, password]
 			let itemid = Main.id $ tdsf!!(read (fromJust number)::Int)
-			return $ CurlPostFields ["todo_item["++(show itemid)++"?]"++curlpostdata]
+			return $ CurlPostFields ["todo_item["++show itemid++"?]"++curlpostdata]
 		else return $ CurlPostFields ["todo_item[todo]="++curlpostdata, "todo_item[do_on]="++todays_date]
 	let opts = method_POST ++ [CurlUserPwd $ username++":"++password, curlpostfields]
 	curl <- initialize
@@ -76,14 +76,14 @@ curlpost [todays_date, curlpostdata, apiurl, okresponse, username, password] num
 
 today [todays_date, username, password] = do
 	tdsf <- curlget [todays_date, username, password]
-	putStr $ unlines $ zipWith (\n td -> show n ++ " - " ++ td) [0..] $ map (\td ->  todo td) tdsf --numbering from LYAH
+	putStr $ unlines $ zipWith (\n td -> show n ++ " - " ++ td) [0..] $ map todo tdsf --numbering from LYAH
 
 
-new [todays_date, todo, username, password] = do
+new [todays_date, todo, username, password] = 
 	curlpost [todays_date, todo, "https://teuxdeux.com/api/todo.json", "Added!", username, password] Nothing
 
 
-crossoff [todays_date, number, username, password] = do
+crossoff [todays_date, number, username, password] = 
 	curlpost [todays_date, "[done=1", "https://teuxdeux.com/api/update.json", "Crossed Off!", username, password](Just number)
 
 
@@ -92,7 +92,7 @@ putoff [todays_date, number, username, password] = do
 	curlpost [todays_date, "[do_on]="++tomorrows_date, "https://teuxdeux.com/api/update.json", "Put Off!", username, password] (Just number)
 
 
-moveto [todays_date, number, new_date, username, password] = do
+moveto [todays_date, number, new_date, username, password] = 
 	curlpost [todays_date, "[do_on]="++new_date, "https://teuxdeux.com/api/update.json", "Moved!", username, password] (Just number)
 
 
